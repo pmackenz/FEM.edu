9210447229441d45f1fef7ba81de4d3edd4d5d3d
Peter Mackenzie-Helnwein <pmackenz@uw.edu>
2023-04-12 17:17:40 -0700

    fixes for release 1.0.0

--- VERSION.rst
+++ VERSION.rst
@@ -1,5 +1,5 @@
 .. list-table::
 
-    * - **Version 1.0.0**
+    * - **Version 1.0.1**
       - **last updated:** |today|
 
--- src/femedu/__init__.py
+++ src/femedu/__init__.py
@@ -1,3 +1,3 @@
 """FEM.edu is nonlinear finite element code designed for use in higher education"""
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
